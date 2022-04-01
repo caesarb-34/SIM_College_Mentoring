01215b6531a77d6b5ad2179972ba830c62c011f8
Caesar Bernardo <caesar.bernardo@gmail.com>
2022-04-01 12:36:10 +0000

    "Configured project for deployment."

--- ment/settings.py
+++ ment/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-knvg^*@_fg&gz1-w3(&nbxb^)im6m*p^2z3e0jv!exatku283y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
